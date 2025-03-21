76891fb63b29cee6e4842bee342b760e562b56eb
Dharmaseelan2004 <dharmaseelan12112004@gmail.com>
2025-03-21 14:21:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,67 +1,82 @@
-import './App.css';
-import Banner from './components/Banner';
-import Footer from './components/Footer';
-import Genre from './components/Genre';
-import Language from './components/Language';
-import Nav from './components/Nav';
-import NavBar from './components/NavBar';
-import Platforms from './components/Platforms';
-import Row from './components/Row';
-import requests from './request';
+<html>
+<head>
+<title>My Web Page</title>
+</head>
 
-function App() {
-  return (
-    <>
-   <div className='rel'>
-   <div className="nnn">
-          <Nav/>
-        </div>
-     
-      <div className="aaa">
-        
+<body bgcolor="pink">
   
-  <div className='first'>
-    
-          <NavBar/>
+  <h1 style="text-align:center;">BIO DATA</h1>
+ <img src="22ITR019.JPG" style="width=128px; height:128px; float:right;">
+
+<h1><p><b>Name</b>:Dharmaseelan R</p></h1>
+  <h2>Gmail id:</h2><p>dharmaseelanr.22it@kongu.edu</p>
+
+  <h1><b>Education Details:</b></h1>
+  <table border="2">
+    <tr>
+      <th>Name of the Degree</th>
+      <th>Name of the Institute</th>
+      <th>Year of passed out</th>
+      <th>Percentage</th>
+    </tr>
+    <tr>
+      <td>B.tech IT</td>
+      <td>Kongu Engineering College</td>
+      <td>2026</td>
+      <td>85%</td>
+    </tr>
+    <tr> 
+      <td>HSC</td>
+      <td>Government Higher Secondary School, Namakkal</td>
+      <td>2022</td>
+      <td>87%</td>
+    </tr>
+    <tr>
+      <td>SSLC</td>
+      <td>Government Higher Secondary School, Namakkal</td>
+      <td>2020</td>
+      <td>85%</td>
+    </tr>
+  </table>
+
+  <h2>Domain:</h2>
+  <p>Full Stack Developer</p>
+
+  <h2>Technology:</h2>
+  <p>Web Development, Database Management</p>
+
+  <h2>Known Programming Languages:</h2>
+  <ul>
+    <li>Java</li>
+    <li>Python</li>
+    <li>C</li>
+    <li>C#</li>
+  </ul>
+
+  <h2>Skills:</h2>
+  <p>Problem Solving, Team Collaboration, Communication, Time Management</p>
+
+  <h2>Projects:</h2>
+  <ul>
+    <li>Presented a paper at Kongu Engineering College.</li>
+    <li>Developed and presented a project at our college.</li>
+  </ul>
+
+  <h2>Achievements:</h2>
+  <p>Recipient of the Academic Excellence Award for outstanding performance in HSC.</p>
+
+  <h2>Declaration:</h2>
+  <p>I hereby declare that the above information is true to the best of my knowledge.</p>
+
+  <h2>Personal Details:</h2>
+  <p>Name: DHARMASEELAN R</p>
+  <p>Address: 1/38 A Pirandhagam, Pramathivelur[TK], Namakkal(Dt)</p>
+
+  <div style="text-align: right; margin-right: 20px;">
+    <h2>Signature:</h2>
+    <p>Dharmaseelan R</p>
+    <!-- Add space for a digital or physical signature -->
   </div>
-  
-   <div className='second'>
-  
-    <div className='stick'>
-      
-     <Banner/>
-    </div>
-    <div className='lg'>
-    <img src="https://img10.hotstar.com/image/upload/f_auto,h_148/sources/r1/cms/prod/2744/1602744-t-f24fc98823fa" alt="" />
-            <h3>2023 . 2h 54m . 4 languages . <span>U/A 16+</span> </h3>
-            <p>In a crime-infested town, Kannan bhai and his gang are the reigning powers. To combat this reign and seek revenge, Inspector </p>
-            <h3>Action | Drama | Thriller | Drugs</h3>
-            <button className='btn'>Subscribe to Watch</button>
-            <button className='bb btn'>+</button>
-  
-  
-  
-    </div>
-    <div className='z'>
-            
-      <Row title="Latest releases" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Free-Newly Added" fetchUrl={requests.fetchComedyMovies}/>
-      <Row title="Disney Movies" fetchUrl={requests.fetchDocumentaries}/>
-      <Platforms/>
-      <Language/>
-      <Genre/>
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
-      <Row title="Romantic Movies" fetchUrl={requests.fetchRomanceMovies}/>
-      <Row title="Populer Movies" fetchUrl={requests.fetchDoc}/>
-      <Footer/>
-    </div>
-    
-    
-   </div>
-      </div>
-   </div>
-    </>
-  );
-}
 
-export default App;
+</body>
+</html>
